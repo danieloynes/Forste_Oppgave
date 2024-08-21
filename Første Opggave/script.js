311cd19b977ffee84f60dc54dfc2adabb7e50f22
danieloynes <danielskole07@gmail.com>
2024-08-21 12:28:35 +0200

    1

---
+++ Første Opggave/script.js
@@ -0,0 +1,59 @@
+
+/*const username = "Daniel"
+const password = "123"
+
+let myUsername = prompt("Skriv et brukernavn")
+let myPassword = prompt("Skriv et brukernavn")
+
+if (username == myUsername && password == myPassword) {
+    console.log("Velkommen")
+} else {
+    console.log("Ikke velkommen")
+}
+*/
+
+/*
+const username = "Daniel"
+const password = "123"
+
+let t1 = Number (prompt("t1"))
+let t2 = Number (prompt("t2"))
+let t3 = Number (prompt("t3"))
+
+console.log((t1+t2+t3)/3)
+*/
+
+/*
+const minVerdi = 4;
+let sum = 0;
+for (let i = 1; i < minverdi+1; i++) {
+    console.log(i);
+    sum = sum + i;
+}
+console.log(sum);
+*/
+
+/*
+//let f = 86;
+//let c = (f-32)*5/9;
+//console.log(c);
+
+let c = 30;
+let f = (c*9/5)+32;
+console.log(f);
+*/
+
+let poeng = 30;
+if (poeng<=30) {
+    console.log("Karakteren din er 1");
+} else if (poeng<=45) {
+    console.log("Karakteren din er 2");
+} else if (poeng<=60) {
+    console.log("Karakteren din er 3");
+} else if (poeng<=75) {
+    console.log("Karakteren din er 4");
+} else if (poeng<=90) {
+    console.log("Karakteren din er 5");
+} else if (poeng<=100){
+    console.log("Karakteren din er 6");
+}
